d23eef3d9db3f1a3ba9459e3c6017396c0846052
Mr0maks <mr.maks0443@gmail.com>
2020-12-29 02:50:52 +0500

    full riscv32i 0x0C opcodes implementation + register dump

--- src/riscv32i.c
+++ src/riscv32i.c
@@ -20,6 +20,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include "riscv.h"
 #include "riscv32.h"
+#include "riscv32i_registers.h"
 
 // translate register number into abi name
 const char *riscv32i_translate_register_interger(uint32_t reg)
@@ -83,53 +84,61 @@ void riscv32i_emulate_0x04(risc32_vm_state_t *vm, uint32_t instruction)
     uint32_t rds = ((instruction >> 7) & 0x1f);
     uint32_t func3 = ((instruction >> 12) & 0x3);
     uint32_t rs1 = ((instruction >> 15) & 0x1f);
-    uint32_t imm = (instruction >> 20);
+    int32_t imm = 0;
 
-    switch (func3) {
+    if( ((instruction >> 20) & 0x800) )
+    {
+        imm = (0xFFFFF000) | (instruction >> 20);
+    } else {
+        imm = (instruction >> 20);
+    }
+
+    switch (func3)
+    {
     case 0x00:
     {
-        printf("addi %u,%u,%u\n", rds, rs1, imm);
+        printf("addi %u,%u,%i\n", rds, rs1, imm);
         break;
     }
     case 0x01:
     {
         if( imm == 0 )
-            printf("slli %u,%u,%u\n", rds, rs1, imm);
+            printf("slli %u,%u,%i\n", rds, rs1, imm);
         break;
     }
     case 0x02:
     {
-        printf("slti %u,%u,%u\n", rds, rs1, imm);
+        printf("slti %u,%u,%i\n", rds, rs1, imm);
         break;
     }
     case 0x03:
     {
-        printf("sltiu %u,%u,%u\n", rds, rs1, imm);
+        printf("sltiu %u,%u,%i\n", rds, rs1, imm);
         break;
     }
     case 0x04:
     {
-        printf("xori %u,%u,%u\n", rds, rs1, imm);
+        printf("xori %u,%u,%i\n", rds, rs1, imm);
         break;
     }
     case 0x05:
     {
         if( imm == 0 ) {
-            printf("srli %u,%u,%u\n", rds, rs1, imm);
+            printf("srli %u,%u,%i\n", rds, rs1, imm);
         } else if( imm == 16 )
         {
-            printf("srai %u,%u,%u\n", rds, rs1, imm);
+            printf("srai %u,%u,%i\n", rds, rs1, imm);
         }
         break;
     }
     case 0x06:
     {
-        printf("ori %u,%u,%u\n", rds, rs1, imm);
+        printf("ori %u,%u,%i\n", rds, rs1, imm);
         break;
     }
     case 0x07:
     {
-        printf("andi %u,%u,%u\n", rds, rs1, imm);
+        printf("andi %u,%u,%i\n", rds, rs1, imm);
         break;
     }
 
@@ -140,7 +149,7 @@ void riscv32i_emulate_0x05(risc32_vm_state_t *vm, uint32_t instruction)
 {
     uint32_t rds = ((instruction >> 7) & 0x1f);
     uint32_t imm = (instruction >> 12);
-    printf("auipc %u, %i\n", rds, imm);
+    printf("auipc %u,%i\n", rds, imm);
 }
 
 void riscv32i_emulate_0x08(risc32_vm_state_t *vm, uint32_t instruction)
@@ -161,47 +170,110 @@ void riscv32i_emulate_0x0C(risc32_vm_state_t *vm, uint32_t instruction)
     case 0:
     {
         if(func7 == 0x20)
-            printf("sub %u,%u,%u\n", rds, rs1, rs2);
+        {
+            int32_t reg1 = riscv32i_read_register_s(vm, rs1), reg2 = riscv32i_read_register_s(vm, rs2);
+
+            int32_t result = (reg1 - reg2);
+            riscv32i_write_register_s(vm, rds, result);
+            //printf("sub %u,%u,%u\n", rds, rs1, rs2);
+        }
         else
-            printf("add %u,%u,%u\n", rds, rs1, rs2);
+        {
+            int32_t reg1 = riscv32i_read_register_s(vm, rs1), reg2 = riscv32i_read_register_s(vm, rs2);
+
+            int32_t result = (reg1 + reg2);
+            riscv32i_write_register_s(vm, rds, result);
+            //printf("add %u,%u,%u\n", rds, rs1, rs2);
+        }
         break;
     }
     case 1:
     {
-        printf("sll %u,%u,%u\n", rds, rs1, rs2);
+        //NOTE: check for ub here?
+        uint32_t reg1 = riscv32i_read_register_u(vm, rs1), reg2 = riscv32i_read_register_u(vm, rs2);
+
+        uint32_t result = (reg1 << reg2);
+
+        riscv32i_write_register_u(vm, rds, result);
+        //printf("sll %u,%u,%u\n", rds, rs1, rs2);
         break;
     }
     case 2:
     {
-        printf("slt %u,%u,%u\n", rds, rs1, rs2);
+        int32_t reg1 = riscv32i_read_register_s(vm, rs1), reg2 = riscv32i_read_register_s(vm, rs2);
+
+        int32_t result = 0;
+        if( reg1 < reg2 )
+            result = 1;
+
+        riscv32i_write_register_s(vm, rds, result);
+        //printf("slt %u,%u,%u\n", rds, rs1, rs2);
         break;
     }
     case 3:
     {
-        printf("sltu %u,%u,%u\n", rds, rs1, rs2);
+        uint32_t reg1 = riscv32i_read_register_u(vm, rs1), reg2 = riscv32i_read_register_u(vm, rs2);
+
+        uint32_t result = 0;
+        if( reg1 < reg2 )
+            result = 1;
+
+        riscv32i_write_register_u(vm, rds, result);
+
+        //printf("sltu %u,%u,%u\n", rds, rs1, rs2);
         break;
     }
     case 4:
     {
+        uint32_t reg1 = riscv32i_read_register_u(vm, rs1), reg2 = riscv32i_read_register_u(vm, rs2);
+
+        uint32_t result = (reg1 ^ reg2);
+
+        riscv32i_write_register_u(vm, rds, result);
         printf("xor %u,%u,%u\n", rds, rs1, rs2);
         break;
     }
     case 5:
     {
         if(func7 == 0x20)
-            printf("sra %u,%u,%u\n", rds, rs1, rs2);
-        else
-            printf("srl %u,%u,%u\n", rds, rs1, rs2);
+        {
+            //NOTE: check for ub here?
+            int32_t reg1 = riscv32i_read_register_s(vm, rs1), reg2 = riscv32i_read_register_s(vm, rs2);
+
+            int32_t result = (reg1 >> reg2);
+
+            riscv32i_write_register_s(vm, rds, result);
+            //printf("sra %u,%u,%u\n", rds, rs1, rs2);
+        } else
+        {
+            //NOTE: check for ub here?
+            uint32_t reg1 = riscv32i_read_register_u(vm, rs1), reg2 = riscv32i_read_register_u(vm, rs2);
+
+            uint32_t result = (reg1 >> reg2);
+
+            riscv32i_write_register_u(vm, rds, result);
+            //printf("srl %u,%u,%u\n", rds, rs1, rs2);
+        }
         break;
     }
     case 6:
     {
-        printf("or %u,%u,%u\n", rds, rs1, rs2);
+        uint32_t reg1 = riscv32i_read_register_u(vm, rs1), reg2 = riscv32i_read_register_u(vm, rs2);
+
+        uint32_t result = (reg1 | reg2);
+
+        riscv32i_write_register_u(vm, rds, result);
+        //printf("or %u,%u,%u\n", rds, rs1, rs2);
         break;
     }
     case 7:
     {
-        printf("and %u,%u,%u\n", rds, rs1, rs2);
+        uint32_t reg1 = riscv32i_read_register_u(vm, rs1), reg2 = riscv32i_read_register_u(vm, rs2);
+
+        uint32_t result = (reg1 & reg2);
+
+        riscv32i_write_register_u(vm, rds, result);
+        //printf("and %u,%u,%u\n", rds, rs1, rs2);
         break;
     }
     }
@@ -210,8 +282,11 @@ void riscv32i_emulate_0x0C(risc32_vm_state_t *vm, uint32_t instruction)
 void riscv32i_emulate_0x0D(risc32_vm_state_t *vm, uint32_t instruction)
 {
     uint32_t rds = ((instruction >> 7) & 0x1f);
-    uint32_t imm = (instruction >> 12);
-    printf("lui %u, %u\n", rds, imm);
+    uint32_t imm = (instruction & 0xFFFFF000);
+
+    riscv32i_write_register_u(vm, rds, imm);
+
+    printf("lui %u,%u\n", rds, imm);
 }
 
 void riscv32i_emulate_0x18(risc32_vm_state_t *vm, uint32_t instruction)
@@ -285,6 +360,18 @@ void risv32i_emulate(risc32_vm_state_t *vm, uint32_t instruction)
         riscv32i_emulate_0x1B(vm, instruction);
         break;
     }
+    }
+
+    int i = 0;
+    int k = 0;
+
+    for( i = 0; i < REGISTERS_MAX; i++ )
+    {
+        for(k = 0; k < 2; i++,k++)
+        {
+            printf("%s: (0x%X %i) ", riscv32i_translate_register_interger(i), riscv32i_read_register_u(vm, i), riscv32i_read_register_s(vm, i));
+        }
+        printf("%s: (0x%X %i)\n", riscv32i_translate_register_interger(i), riscv32i_read_register_u(vm, i), riscv32i_read_register_s(vm, i));
     }
 
     vm->registers[REGISTER_PC]++;
--- src/riscv32i_registers.h
+++ src/riscv32i_registers.h
@@ -2,7 +2,7 @@
 
 #include "riscv32.h"
 
-inline uint32_t riscv32i_read_register(risc32_vm_state_t *vm, uint32_t reg)
+inline uint32_t riscv32i_read_register_u(risc32_vm_state_t *vm, uint32_t reg)
 {
     assert(reg < REGISTERS_MAX);
 
@@ -13,7 +13,29 @@ inline uint32_t riscv32i_read_register(risc32_vm_state_t *vm, uint32_t reg)
     return vm->registers[reg];
 }
 
-inline void riscv32i_write_register(risc32_vm_state_t *vm, uint32_t reg, uint32_t data)
+inline void riscv32i_write_register_u(risc32_vm_state_t *vm, uint32_t reg, uint32_t data)
+{
+    assert(reg < REGISTERS_MAX);
+
+    // always ignore for x0
+    if(reg == REGISTER_X0)
+        return;
+
+    vm->registers[reg] = data;
+}
+
+inline int32_t riscv32i_read_register_s(risc32_vm_state_t *vm, uint32_t reg)
+{
+    assert(reg < REGISTERS_MAX);
+
+    // always return 0 for x0
+    if(reg == REGISTER_X0)
+        return 0;
+
+    return vm->registers[reg];
+}
+
+inline void riscv32i_write_register_s(risc32_vm_state_t *vm, uint32_t reg, int32_t data)
 {
     assert(reg < REGISTERS_MAX);
 
