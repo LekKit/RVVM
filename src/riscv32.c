85ada9b2f9dbfcfb1ac272b3e4aa0fc2503bfb93
LekKit <50500857+LekKit@users.noreply.github.com>
2020-12-30 05:34:48 +0200

    Better decoder, vm creation/destruction methods

--- src/riscv32.c
+++ src/riscv32.c
@@ -19,31 +19,45 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "riscv.h"
 #include "riscv32.h"
 #include "riscv32i.h"
 #include "riscv32c.h"
+#include "mem_ops.h"
 
-static inline uint32_t riscv32_fetch_insn32(risc32_vm_state_t *vm)
+risc32_vm_state_t *riscv32_create_vm()
 {
-    // Read 32-bit width instruction
-    return ((uint32_t)vm->code[vm->code_pointer]) << 16 | vm->code[vm->code_pointer+1];
+    static bool global_init = false;
+    if (!global_init) {
+        riscv32c_init();
+        global_init = true;
+    }
+
+    risc32_vm_state_t *vm = (risc32_vm_state_t*)malloc(sizeof(risc32_vm_state_t));
+    memset(vm, 0, sizeof(risc32_vm_state_t));
+    // Put other stuff here
+
+    return vm;
 }
 
-void riscv32_exec_instuction(risc32_vm_state_t *vm)
+void riscv32_destroy_vm(risc32_vm_state_t *vm)
 {
-    uint32_t instruction = riscv32_fetch_insn32(vm);
+    free(vm);
+}
 
+void riscv32_exec_instruction(risc32_vm_state_t *vm)
+{
     // FYI: Any jump instruction implementation should take care of PC increment
     // TODO: proper error handling (maybe not here)
-    if ((instruction & RISCV32I_OPCODE_MASK) != RISCV32I_OPCODE_MASK) {
+    if ((vm->code[vm->code_pointer] & RISCV32I_OPCODE_MASK) != RISCV32I_OPCODE_MASK) {
         // 16-bit opcode
-        riscv32c_emulate(vm, vm->code[vm->code_pointer]);
-        vm->code_pointer++;
-    } else {
-        riscv32i_emulate(vm, instruction);
+        riscv32c_emulate(vm, read_uint16_le(vm->code+vm->code_pointer));
         vm->code_pointer += 2;
+    } else {
+        riscv32i_emulate(vm, read_uint32_le(vm->code+vm->code_pointer));
+        vm->code_pointer += 4;
     }
 }
 
@@ -59,6 +73,6 @@ void riscv32_run(risc32_vm_state_t *vm)
 
     while (vm->code_pointer != vm->code_len)
     {
-        riscv32_exec_instuction(vm);
+        riscv32_exec_instruction(vm);
     }
 }
