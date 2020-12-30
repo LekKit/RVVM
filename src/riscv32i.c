89f198de977854e7fc52a380067c81148212ccd2
LekKit <50500857+LekKit@users.noreply.github.com>
2020-12-30 14:13:15 +0200

    Create prototypes for RV32I instructions

--- src/riscv32i.c
+++ src/riscv32i.c
@@ -304,9 +304,231 @@ void riscv32i_emulate_0x1B(risc32_vm_state_t *vm, uint32_t instruction)
     printf("riscv32i 0x1b opcode\n");
 }
 
+static void riscv32i_lui(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: LUI instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_auipc(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: AUIPC instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_jal(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: JAL instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_srli_srai(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: SRLI/SRAI instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_add_sub(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: ADD/SUB instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_ecall_ebreak(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: ECALL/EBREAK instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_srl_sra(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: SRL/SRA instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_jalr(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: JALR instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_beq(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: BEQ instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_bne(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: BNE instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_blt(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: BLT instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_bge(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: BGE instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_bltu(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: BLTU instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_bgeu(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: BGEU instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_lb(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: LB instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_lh(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: LH instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_lw(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: LW instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_lbu(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: LBU instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_lhu(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: LHU instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_sb(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: SB instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_sh(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: SH instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_sw(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: SW instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_addi(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: ADDI instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_slti(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: SLTI instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_sltiu(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: SLTIU instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_xori(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: XORI instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_ori(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: ORI instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_andi(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: ANDI instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_slli(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: SLLI instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_sll(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: SLL instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_slt(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: SLT instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_sltu(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: SLTU instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_xor(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: XOR instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_or(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: OR instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_and(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: AND instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_fence(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: FENCE instruction 0x%x in VM %p\n", instruction, vm);
+}
+
+static void riscv32i_(risc32_vm_state_t *vm, uint32_t instruction)
+{
+    printf("RV32I: AAA instruction 0x%x in VM %p\n", instruction, vm);
+}
+
 void riscv32i_init()
 {
+    smudge_opcode_func3(RV32I_LUI, riscv32i_lui);
+    smudge_opcode_func3(RV32I_AUIPC, riscv32i_auipc);
+    smudge_opcode_func3(RV32I_JAL, riscv32i_jal);
 
+    riscv32_opcodes[RV32I_SRLI_SRAI] = riscv32i_srli_srai;
+    riscv32_opcodes[RV32I_ADD_SUB] = riscv32i_add_sub;
+    riscv32_opcodes[RV32I_ECALL_EBREAK] = riscv32i_ecall_ebreak;
+    riscv32_opcodes[RV32I_SRL_SRA] = riscv32i_srl_sra;
+
+    riscv32_opcodes[RV32I_JALR] = riscv32i_jalr;
+    riscv32_opcodes[RV32I_BEQ] = riscv32i_beq;
+    riscv32_opcodes[RV32I_BNE] = riscv32i_bne;
+    riscv32_opcodes[RV32I_BLT] = riscv32i_blt;
+    riscv32_opcodes[RV32I_BGE] = riscv32i_bge;
+    riscv32_opcodes[RV32I_BLTU] = riscv32i_bltu;
+    riscv32_opcodes[RV32I_BGEU] = riscv32i_bgeu;
+    riscv32_opcodes[RV32I_LB] = riscv32i_lb;
+    riscv32_opcodes[RV32I_LH] = riscv32i_lh;
+    riscv32_opcodes[RV32I_LW] = riscv32i_lw;
+    riscv32_opcodes[RV32I_LBU] = riscv32i_lbu;
+    riscv32_opcodes[RV32I_LHU] = riscv32i_lhu;
+    riscv32_opcodes[RV32I_SB] = riscv32i_sb;
+    riscv32_opcodes[RV32I_SH] = riscv32i_sh;
+    riscv32_opcodes[RV32I_SW] = riscv32i_sw;
+    riscv32_opcodes[RV32I_ADDI] = riscv32i_addi;
+    riscv32_opcodes[RV32I_SLTI] = riscv32i_slti;
+    riscv32_opcodes[RV32I_SLTIU] = riscv32i_sltiu;
+    riscv32_opcodes[RV32I_XORI] = riscv32i_xori;
+    riscv32_opcodes[RV32I_ORI] = riscv32i_ori;
+    riscv32_opcodes[RV32I_ANDI] = riscv32i_andi;
+    riscv32_opcodes[RV32I_SLLI] = riscv32i_slli;
+    riscv32_opcodes[RV32I_SLL] = riscv32i_sll;
+    riscv32_opcodes[RV32I_SLT] = riscv32i_slt;
+    riscv32_opcodes[RV32I_SLTU] = riscv32i_sltu;
+    riscv32_opcodes[RV32I_XOR] = riscv32i_xor;
+    riscv32_opcodes[RV32I_OR] = riscv32i_or;
+    riscv32_opcodes[RV32I_AND] = riscv32i_and;
+    riscv32_opcodes[RV32I_FENCE] = riscv32i_fence;
 }
 
 // We already check instruction for correct code
