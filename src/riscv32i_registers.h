fd2305bdecdfbc129caac7c341363022c83e472c
Mr0maks <mr.maks0443@gmail.com>
2021-01-02 18:19:32 +0500

    remove zero register checks

--- src/riscv32i_registers.h
+++ src/riscv32i_registers.h
@@ -5,11 +5,6 @@
 inline uint32_t riscv32i_read_register_u(risc32_vm_state_t *vm, uint32_t reg)
 {
     assert(reg < REGISTERS_MAX);
-
-    // always return 0 for x0
-    if(reg == REGISTER_X0)
-        return 0;
-
     return vm->registers[reg];
 }
 
@@ -27,11 +22,6 @@ inline void riscv32i_write_register_u(risc32_vm_state_t *vm, uint32_t reg, uint3
 inline int32_t riscv32i_read_register_s(risc32_vm_state_t *vm, uint32_t reg)
 {
     assert(reg < REGISTERS_MAX);
-
-    // always return 0 for x0
-    if(reg == REGISTER_X0)
-        return 0;
-
     return vm->registers[reg];
 }
 
