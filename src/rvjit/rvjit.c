b15aedb965afc08321bec89c04f020f592b48dd9
LekKit <50500857+LekKit@users.noreply.github.com>
2021-03-27 13:51:29 +0200

    JIT + x86 codegen

---
+++ src/rvjit/rvjit.c
@@ -0,0 +1,109 @@
+/*
+rvjit.c - Retargetable Versatile JIT Compiler
+Copyright (C) 2021  LekKit <github.com/LekKit>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "rvjit.h"
+#include "rvjit_emit.h"
+
+#include "rvjit_x86.h"
+
+#ifdef _WIN32
+#include <windows.h>
+#include <memoryapi.h>
+#else
+#include <sys/mman.h>
+#endif
+
+void rvjit_heap_init(rvjit_heap_t* heap, size_t size_kb)
+{
+    size_t size = ((size_kb + 3) & ~3) << 10;
+#ifdef _WIN32
+#ifdef RVJIT_PARANOID
+    heap->data = VirtualAlloc(NULL, size, MEM_COMMIT, PAGE_READWRITE);
+#else
+    heap->data = VirtualAlloc(NULL, size, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+#endif
+#else
+#ifdef RVJIT_PARANOID
+    heap->data = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+#else
+    heap->data = mmap(NULL, size, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+#endif
+#endif
+    heap->size = size;
+    heap->curr = 0;
+}
+
+void rvjit_heap_free(rvjit_heap_t* heap)
+{
+#ifdef _WIN32
+    VirtualFree(heap->data, 0, MEM_RELEASE);
+#else
+    munmap(heap->data, heap->size);
+#endif
+}
+
+static void rvjit_heap_protect(rvjit_heap_t* heap)
+{
+#ifdef RVJIT_PARANOID
+#ifdef _WIN32
+    DWORD old;
+    VirtualProtect(heap->data, heap->size, PAGE_EXECUTE_READ, &old);
+#else
+    mprotect(heap->data, heap->size, PROT_READ | PROT_EXEC);
+#endif
+#endif
+}
+
+static void rvjit_heap_unprotect(rvjit_heap_t* heap)
+{
+#ifdef RVJIT_PARANOID
+#ifdef _WIN32
+    DWORD old;
+    VirtualProtect(heap->data, heap->size, PAGE_READWRITE, &old);
+#else
+    mprotect(heap->data, heap->size, PROT_READ | PROT_WRITE);
+#endif
+#endif
+}
+
+void rvjit_block_init(rvjit_block_t* block, rvjit_heap_t* heap)
+{
+    block->heap = heap;
+    block->code = ((code_ptr_t)heap->data) + heap->curr;
+    block->size = 0;
+    block->hreg_mask = rvjit_native_default_hregmask();
+    block->abireclaim_mask = 0;
+    for (regid_t i=0; i<REGISTERS_MAX; ++i) {
+        block->regs[i].hreg = REG_ILL;
+        block->regs[i].last_used = 0;
+        block->regs[i].flags = 0;
+    }
+    rvjit_heap_unprotect(block->heap);
+}
+
+code_ptr_t rvjit_block_finish(rvjit_block_t* block)
+{
+    rvjit_emit_end(block);
+    rvjit_heap_protect(block->heap);
+    // Flush instruction cache
+    #ifndef _WIN32
+    __builtin___clear_cache(block->code, block->code + block->size);
+    #endif
+    block->heap->curr += (block->size + 7) & ~7;
+    return block->code;
+}
---
+++ src/rvjit/rvjit.h
@@ -0,0 +1,126 @@
+/*
+rvjit.h - Retargetable Versatile JIT Compiler
+Copyright (C) 2021  LekKit <github.com/LekKit>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <string.h>
+#include <setjmp.h>
+#include <assert.h>
+
+#ifndef RVJIT_H
+#define RVJIT_H
+
+// Explicitly protect/unprotect memory on each JIT invocation
+#define RVJIT_PARANOID
+
+#define REG_ILL 0xFF // Register is not allocated
+
+#define REGISTERS_MAX 32
+#define REGISTER_ZERO_ENABLED 1
+
+#define REG_SRC    0x1
+#define REG_DST    0x2
+
+#define REG_LOADED REG_SRC
+#define REG_DIRTY  REG_DST
+
+#ifdef __x86_64__
+    #ifdef _WIN32
+        #ifdef __MINGW64__
+            #define RVJIT_CALL __attribute__((sysv_abi))
+            #define RVJIT_ABI_SYSV 1
+        #else
+            #define RVJIT_ABI_WIN64 1
+        #endif
+    #else
+        #define RVJIT_CALL __attribute__((sysv_abi))
+        #define RVJIT_ABI_SYSV 1
+    #endif
+    #define RVJIT_NATIVE_64BIT 1
+    #define RVJIT_X86 1
+#elif __i386__
+    #ifdef _WIN32
+        #ifdef __MINGW32__
+            #define RVJIT_CALL __attribute__((fastcall))
+        #else
+            #define RVJIT_CALL __fastcall
+        #endif
+    #else
+        #define RVJIT_CALL __attribute__((fastcall))
+    #endif
+    #define RVJIT_ABI_FASTCALL 1
+    #define RVJIT_X86 1
+#else
+    #error No JIT support for the target platform!!!
+#endif
+
+// No specific calling convention requirements
+#ifndef RVJIT_CALL
+#define RVJIT_CALL
+#endif
+
+typedef RVJIT_CALL void (*code_ptr_t)(void* vm);
+typedef uint8_t regid_t;
+typedef uint8_t regflags_t;
+
+typedef struct {
+    void* data;
+    //uint8_t* bitmask;
+    //uint32_t* block_sizes;
+    size_t curr;
+    size_t size;
+} rvjit_heap_t;
+
+typedef struct {
+    size_t last_used;   // Last usage of register for LRU reclaim
+    regid_t hreg;       // Claimed host register, REG_ILL if not mapped
+    regflags_t flags;   // Register allocation details
+} rvjit_reginfo_t;
+
+typedef struct {
+    rvjit_heap_t* heap;
+    code_ptr_t code;
+    size_t size;
+    size_t hreg_mask;        // Bitmask of available non-clobbered host registers
+    size_t abireclaim_mask;  // Bitmask of reclaimed abi-clobbered host registers to restore
+    rvjit_reginfo_t regs[REGISTERS_MAX];
+} rvjit_block_t;
+
+void rvjit_heap_init(rvjit_heap_t* heap, size_t size_kb);
+void rvjit_heap_free(rvjit_heap_t* heap);
+void rvjit_block_init(rvjit_block_t* block, rvjit_heap_t* heap);
+code_ptr_t rvjit_block_finish(rvjit_block_t* block);
+
+static inline size_t rvjit_hreg_mask(regid_t hreg)
+{
+    return (1ULL << hreg);
+}
+
+static inline void rvjit_put_code(rvjit_block_t* block, const void* inst, size_t size)
+{
+    memcpy(block->code + block->size, inst, size);
+    block->size += size;
+}
+
+static inline void rvjit_exec(void* vm, rvjit_block_t* block)
+{
+    block->code(vm);
+}
+
+#endif
