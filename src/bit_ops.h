6dd4c7d236a320d90442bb6c02d203ef46bbc9b3
LekKit <50500857+LekKit@users.noreply.github.com>
2021-03-30 06:49:15 +0300

    Add 64-bit support to bit_ops

--- src/bit_ops.h
+++ src/bit_ops.h
@@ -21,53 +21,57 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #define RISCV_BIT_OPS_H
 
 #include <stdint.h>
+#include <stddef.h>
 #include <stdbool.h>
+#include <limits.h>
 
 // Simple math operations (sign-extend int bits, etc) for internal usage
 // this entire header is cursed (though everything optimizes fine)
 
+#define LONG_BITS (CHAR_BIT * sizeof(long))
+
 /*
-* Sign-extend bits in the lower part of uint32_t into signed int32_t
+* Sign-extend bits in the lower part of val into signed long
 * usage:
-*     int32_t ext = sign_extend(val, 20);
+*     long ext = sign_extend(val, 20);
 *
 *     [ext is now equal to signed lower 20 bits of val]
 */
-inline int32_t sign_extend(uint32_t val, uint32_t bits)
+inline long sign_extend(long val, uint8_t bits)
 {
-    return ((int32_t)(val << (32 - bits))) >> (32 - bits);
+    return (val << (LONG_BITS - bits)) >> (LONG_BITS - bits);
 }
 
 // Generate bitmask of given size
-inline uint32_t bit_mask(uint32_t count)
+inline size_t bit_mask(uint8_t count)
 {
-    return 0xFFFFFFFF >> (32 - count);
+    return (1UL << count) - 1;
 }
 
 // Cut N bits from val at given position (from lower bit)
-inline uint32_t bit_cut(uint32_t val, uint32_t pos, uint32_t count)
+inline size_t bit_cut(size_t val, uint8_t pos, uint8_t count)
 {
     return (val >> pos) & bit_mask(count);
 }
 
 // Replace N bits in val at given position (from lower bit) by p
-inline uint32_t bit_replace(uint32_t val, uint32_t pos, uint32_t bits, uint32_t p)
+inline size_t bit_replace(size_t val, uint8_t pos, uint8_t bits, size_t p)
 {
     return (val & (~(bit_mask(bits) << pos))) | ((p & bit_mask(bits)) << pos);
 }
 
 // Check if Nth bit of val is 1
-inline bool bit_check(uint32_t val, uint32_t pos)
+inline bool bit_check(size_t val, uint8_t pos)
 {
     return (val >> pos) & 0x1;
 }
 
 // Reverse N bits in val (from lower bit), remaining bits are zero
-inline uint32_t bit_reverse(uint32_t val, uint32_t bits)
+inline size_t bit_reverse(size_t val, uint8_t bits)
 {
-    uint32_t ret = 0;
+    size_t ret = 0;
 
-    for (uint32_t i=0; i<bits; ++i) {
+    for (uint8_t i=0; i<bits; ++i) {
         ret <<= 1;
         ret |= val & 0x1;
         val >>= 1;
