81d9422a5e1c6d7f965698901826de7c82b9fd70
Sergey Larin <cerg2010cerg2010@mail.ru>
2021-05-18 23:38:10 +0300

    X11 fixes
    Fix for XShm error path - previously if XShm wasn't available
    the framebuffer wouldn't start working at all.
    
    16-bit screens are working now - previously running on it resulted
    in crash from pixel conversion function.
    
    Also changes error handling in fb_create_window.

--- src/devices/fb_window.c
+++ src/devices/fb_window.c
@@ -20,41 +20,57 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include "riscv32_mmu.h"
 #include "fb_window.h"
 
-void r5g6b5_to_r8g8b8(const void* _in, void* _out, size_t length)
+void r5g6b5_to_a8r8g8b8(const void* _in, void* _out, size_t length)
 {
-	const uint8_t* in = (uint8_t*)_in;
-	uint8_t* out = (uint8_t*)_out;
-	for (size_t i=0; i<length; ++i) {
-		uint8_t r5 = in[i*2] & 31;
-		uint8_t g6 = ((in[i*2] >> 5) | (in[i*2 + 1] << 3)) & 63;
-		uint8_t b5 = in[i*2 + 1] >> 3;
+    const uint8_t* in = (uint8_t*)_in;
+    uint8_t* out = (uint8_t*)_out;
+    for (size_t i=0; i<length; ++i) {
+        uint8_t r5 = in[i*2] & 31;
+        uint8_t g6 = ((in[i*2] >> 5) | (in[i*2 + 1] << 3)) & 63;
+        uint8_t b5 = in[i*2 + 1] >> 3;
+	if (r5 || g6 || b5)
+		printf("OK SCREEN\n");
 
-		out[i*4] = (r5 << 3) | (r5 >> 2);
-		out[i*4 + 1] = (g6 << 2) | (g6 >> 4);
-		out[i*4 + 2] = (b5 << 3) | (b5 >> 2);
-		out[i*4 + 3] = 0;
-	}
+        out[i*4] = (r5 << 3) | (r5 >> 2);
+        out[i*4 + 1] = (g6 << 2) | (g6 >> 4);
+        out[i*4 + 2] = (b5 << 3) | (b5 >> 2);
+        out[i*4 + 3] = 0xff;
+    }
+}
+
+void a8r8g8b8_to_r5g6b5(const void* _in, void* _out, size_t length)
+{
+    const uint8_t* in = (uint8_t*)_in;
+    uint8_t* out = (uint8_t*)_out;
+    for (size_t i=0; i<length; ++i) {
+	    uint8_t r5 = in[i*4] >> 3;
+	    uint8_t g6 = in[i*4 + 1] >> 2;
+	    uint8_t b5 = in[i*4 + 2] >> 3;
+
+	    out[i * 2] = (g6 << 5) | b5;
+	    out[i * 2 + 1] = (r5 << 3) | (g6 >> 3);
+    }
 }
 
 static bool fb_mmio_handler(riscv32_vm_state_t* vm, riscv32_mmio_device_t* device, uint32_t offset, void* data, uint32_t size, uint8_t op)
 {
-	char* devptr = ((char*)device->data) + offset;
-	char* dataptr = (char*)data;
+    char* devptr = ((char*)device->data) + offset;
+    char* dataptr = (char*)data;
 
-	char *destptr;
-	const char *srcptr;
+    char *destptr;
+    const char *srcptr;
 
-	UNUSED(vm);
-	if (op == MMU_WRITE) {
-		destptr = devptr;
-		srcptr = dataptr;
-	} else {
-		destptr = dataptr;
-		srcptr = devptr;
-	}
+    UNUSED(vm);
+    if (op == MMU_WRITE) {
+        destptr = devptr;
+        srcptr = dataptr;
+    } else {
+        destptr = dataptr;
+        srcptr = devptr;
+    }
 
-	memcpy(destptr, srcptr, size);
-	return true;
+    memcpy(destptr, srcptr, size);
+    return true;
 }
 
 void init_fb(riscv32_vm_state_t* vm, struct fb_data *data, unsigned width, unsigned height, uint32_t addr, struct ps2_device *mouse, struct ps2_device *keyboard)
@@ -63,8 +79,9 @@ void init_fb(riscv32_vm_state_t* vm, struct fb_data *data, unsigned width, unsig
     data->mouse = mouse;
     data->keyboard = keyboard;
     fb_create_window(data, width, height, "RVVM");
-    if (data->framebuffer != NULL)
-    {
-	    riscv32_mmio_add_device(vm, addr, addr + fb_size, fb_mmio_handler, data->framebuffer);
+    if (data->framebuffer == NULL) {
+        /* Initialize empty buffer so that the MMIO handler would not crash */
+        data->framebuffer = calloc(1, fb_size);
     }
+    riscv32_mmio_add_device(vm, addr, addr + fb_size, fb_mmio_handler, data->framebuffer);
 }
