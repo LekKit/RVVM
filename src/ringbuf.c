e4fa2eb9e9b6231dd31597ecec9b138d164fbaf7
Sergey Larin <cerg2010cerg2010@mail.ru>
2021-07-27 22:36:07 +0300

    Fix ringbuf include

--- src/ringbuf.c
+++ src/ringbuf.c
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-#include <ringbuf.h>
+#include "ringbuf.h"
 #include <utils.h>
 #include <string.h>
 
