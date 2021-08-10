8560b1257222486a92fe960fedd96ab8e4eae0b4
LekKit <50500857+LekKit@users.noreply.github.com>
2021-08-10 09:38:53 +0300

    Fix alignment macros

--- src/riscv_mmu.h
+++ src/riscv_mmu.h
@@ -133,14 +133,14 @@ static inline bool riscv_block_in_page(addr_t addr, size_t size)
     return (addr & PAGE_MASK) + size <= PAGE_SIZE;
 }
 
-static inline bool riscv_block_aligned(addr_t addr, uint8_t size)
+static inline bool riscv_block_aligned(addr_t addr, size_t size)
 {
     return (addr & (size - 1)) == 0;
 }
 
-static inline addr_t riscv_align_addr(addr_t addr, uint8_t size)
+static inline addr_t riscv_align_addr(addr_t addr, size_t size)
 {
-    return addr & (size - 1);
+    return addr & (~(addr_t)(size - 1));
 }
 
 // VM Address translation (translated within single page bounds)
