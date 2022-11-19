afdaf0641ab2e444080936e59e857ea45ca8811d
LekKit <50500857+LekKit@users.noreply.github.com>
2022-11-19 21:08:19 +0200

    Rewrite Win32 window using new HID/FB APIs
    - Remove hashmap-based keymap, use a table. This is much better since both HID & win32 keycodes are small in size
    - Improve error checking on dangerous winapi calls
    - Fix F10 keypresses (Win32 somehow invented another type of event for this specific button, huh)
    - Implement scroll wheel & middle button
    - General refactoring

--- src/devices/win32window.c
+++ src/devices/win32window.c
@@ -16,29 +16,18 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-#include "fb_window.h"
-#include "ps2-mouse.h"
-#include "ps2-keyboard.h"
-#include "utils.h"
-
 #ifndef UNICODE
 #define UNICODE
 #endif
+
 #include <windows.h>
 
-#define KEYMAP_PAUSE VK_PAUSE
-#define KEYMAP_PRINT VK_PRINT
+#include "fb_window.h"
+#include "utils.h"
 
-#include "keymap.h"
-
-typedef struct {
+struct win_data {
     HWND hwnd;
-    POINTS lastcur;
-    struct mouse_btns btns;
-    int x;
-    int y;
-    char name[256];
-} win32fb_data;
+};
 
 static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
@@ -53,131 +42,125 @@ static ATOM winclass_atom = 0;
 static uint32_t winborder_x;
 static uint32_t winborder_y;
 
-static void init_keycodes()
-{
-    init_keymap();
+static const hid_key_t win32_key_to_hid_byte_map[] = {
+    [0x41] = HID_KEY_A,
+    [0x42] = HID_KEY_B,
+    [0x43] = HID_KEY_C,
+    [0x44] = HID_KEY_D,
+    [0x45] = HID_KEY_E,
+    [0x46] = HID_KEY_F,
+    [0x47] = HID_KEY_G,
+    [0x48] = HID_KEY_H,
+    [0x49] = HID_KEY_I,
+    [0x4A] = HID_KEY_J,
+    [0x4B] = HID_KEY_K,
+    [0x4C] = HID_KEY_L,
+    [0x4D] = HID_KEY_M,
+    [0x4E] = HID_KEY_N,
+    [0x4F] = HID_KEY_O,
+    [0x50] = HID_KEY_P,
+    [0x51] = HID_KEY_Q,
+    [0x52] = HID_KEY_R,
+    [0x53] = HID_KEY_S,
+    [0x54] = HID_KEY_T,
+    [0x55] = HID_KEY_U,
+    [0x56] = HID_KEY_V,
+    [0x57] = HID_KEY_W,
+    [0x58] = HID_KEY_X,
+    [0x59] = HID_KEY_Y,
+    [0x5A] = HID_KEY_Z,
+    [0x30] = HID_KEY_0,
+    [0x31] = HID_KEY_1,
+    [0x32] = HID_KEY_2,
+    [0x33] = HID_KEY_3,
+    [0x34] = HID_KEY_4,
+    [0x35] = HID_KEY_5,
+    [0x36] = HID_KEY_6,
+    [0x37] = HID_KEY_7,
+    [0x38] = HID_KEY_8,
+    [0x39] = HID_KEY_9,
+    [0x0D] = HID_KEY_ENTER,
+    [0x1B] = HID_KEY_ESC,
+    [0x08] = HID_KEY_BACKSPACE,
+    [0x09] = HID_KEY_TAB,
+    [0x20] = HID_KEY_SPACE,
+    [0xBD] = HID_KEY_MINUS,
+    [0xBB] = HID_KEY_EQUAL,
+    [0xDB] = HID_KEY_LEFTBRACE,
+    [0xDD] = HID_KEY_RIGHTBRACE,
+    [0xDC] = HID_KEY_BACKSLASH,
+    [0xBA] = HID_KEY_SEMICOLON,
+    [0xDE] = HID_KEY_APOSTROPHE,
+    [0xC0] = HID_KEY_GRAVE,
+    [0xBC] = HID_KEY_COMMA,
+    [0xBE] = HID_KEY_DOT,
+    [0xBF] = HID_KEY_SLASH,
+    [0x14] = HID_KEY_CAPSLOCK,
+    [0x11] = HID_KEY_LEFTCTRL,
+    [0x10] = HID_KEY_LEFTSHIFT,
+    [0x12] = HID_KEY_LEFTALT,
+    [0x5B] = HID_KEY_LEFTMETA,
+    [0xA3] = HID_KEY_RIGHTCTRL,
+    [0xA1] = HID_KEY_RIGHTSHIFT,
+    [0xA5] = HID_KEY_RIGHTALT,
+    [0x5C] = HID_KEY_RIGHTMETA,
+    [0x70] = HID_KEY_F1,
+    [0x71] = HID_KEY_F2,
+    [0x72] = HID_KEY_F3,
+    [0x73] = HID_KEY_F4,
+    [0x74] = HID_KEY_F5,
+    [0x75] = HID_KEY_F6,
+    [0x76] = HID_KEY_F7,
+    [0x77] = HID_KEY_F8,
+    [0x78] = HID_KEY_F9,
+    [0x79] = HID_KEY_F10,
+    [0x7A] = HID_KEY_F11,
+    [0x7B] = HID_KEY_F12,
+    [0x2C] = HID_KEY_SYSRQ,
+    [0x91] = HID_KEY_SCROLLLOCK,
+    [0x13] = HID_KEY_PAUSE,
+    [0x2D] = HID_KEY_INSERT,
+    [0x24] = HID_KEY_HOME,
+    [0x21] = HID_KEY_PAGEUP,
+    [0x2E] = HID_KEY_DELETE,
+    [0x23] = HID_KEY_END,
+    [0x22] = HID_KEY_PAGEDOWN,
+    [0x27] = HID_KEY_RIGHT,
+    [0x25] = HID_KEY_LEFT,
+    [0x28] = HID_KEY_DOWN,
+    [0x26] = HID_KEY_UP,
+    [0x90] = HID_KEY_NUMLOCK,
+    [0x6F] = HID_KEY_KPSLASH,
+    [0x6A] = HID_KEY_KPASTERISK,
+    [0x6D] = HID_KEY_KPMINUS,
+    [0x6B] = HID_KEY_KPPLUS,
+    [0x6C] = HID_KEY_KPENTER,
+    [0x61] = HID_KEY_KP1,
+    [0x62] = HID_KEY_KP2,
+    [0x63] = HID_KEY_KP3,
+    [0x64] = HID_KEY_KP4,
+    [0x65] = HID_KEY_KP5,
+    [0x66] = HID_KEY_KP6,
+    [0x67] = HID_KEY_KP7,
+    [0x68] = HID_KEY_KP8,
+    [0x69] = HID_KEY_KP9,
+    [0x60] = HID_KEY_KP0,
+    [0x6E] = HID_KEY_KPDOT,
+    [0x5D] = HID_KEY_MENU,
+};
 
-    init_keycode(0x41, 0x1C, 1);
-    init_keycode(0x42, 0x32, 1);
-    init_keycode(0x43, 0x21, 1);
-    init_keycode(0x44, 0x23, 1);
-    init_keycode(0x45, 0x24, 1);
-    init_keycode(0x46, 0x2B, 1);
-    init_keycode(0x47, 0x34, 1);
-    init_keycode(0x48, 0x33, 1);
-    init_keycode(0x49, 0x43, 1);
-    init_keycode(0x4A, 0x3B, 1);
-    init_keycode(0x4B, 0x42, 1);
-    init_keycode(0x4C, 0x4B, 1);
-    init_keycode(0x4D, 0x3A, 1);
-    init_keycode(0x4E, 0x31, 1);
-    init_keycode(0x4F, 0x44, 1);
-    init_keycode(0x50, 0x4D, 1);
-    init_keycode(0x51, 0x15, 1);
-    init_keycode(0x52, 0x2D, 1);
-    init_keycode(0x53, 0x1B, 1);
-    init_keycode(0x54, 0x2C, 1);
-    init_keycode(0x55, 0x3C, 1);
-    init_keycode(0x56, 0x2A, 1);
-    init_keycode(0x57, 0x1D, 1);
-    init_keycode(0x58, 0x22, 1);
-    init_keycode(0x59, 0x35, 1);
-    init_keycode(0x5A, 0x1A, 1);
-    init_keycode(0x30, 0x45, 1);
-    init_keycode(0x31, 0x16, 1);
-    init_keycode(0x32, 0x1E, 1);
-    init_keycode(0x33, 0x26, 1);
-    init_keycode(0x34, 0x25, 1);
-    init_keycode(0x35, 0x2E, 1);
-    init_keycode(0x36, 0x36, 1);
-    init_keycode(0x37, 0x3D, 1);
-    init_keycode(0x38, 0x3E, 1);
-    init_keycode(0x39, 0x46, 1);
-    init_keycode(0xC0, 0x0E, 1);
-    init_keycode(0xBD, 0x4E, 1);
-    init_keycode(0xBB, 0x55, 1);
-    init_keycode(0xDC, 0x5D, 1);
-    init_keycode(0x08, 0x66, 1);
-    init_keycode(0x20, 0x29, 1);
-    init_keycode(0x09, 0x0D, 1);
-    init_keycode(0x14, 0x58, 1);
-    init_keycode(0x10, 0x12, 1);
-    init_keycode(0x11, 0x14, 1);
-    init_keycode(0x5B, 0x1FE0, 2);
-    init_keycode(0x12, 0x11, 1);
-    init_keycode(0xA1, 0x59, 1);
-    init_keycode(0xA3, 0x14E0, 2);
-    init_keycode(0x5C, 0x27E0, 2);
-    init_keycode(0xA5, 0x11E0, 2);
-    init_keycode(0x5D, 0x2FE0, 2); // APPS)
-    init_keycode(0x0D, 0x5A, 1);
-    init_keycode(0x1B, 0x76, 1);
-    init_keycode(0x70, 0x05, 1);
-    init_keycode(0x71, 0x06, 1);
-    init_keycode(0x72, 0x04, 1);
-    init_keycode(0x73, 0x0C, 1);
-    init_keycode(0x74, 0x03, 1);
-    init_keycode(0x75, 0x0B, 1);
-    init_keycode(0x76, 0x83, 1);
-    init_keycode(0x77, 0x0A, 1);
-    init_keycode(0x78, 0x01, 1);
-    init_keycode(0x79, 0x09, 1);
-    init_keycode(0x7A, 0x78, 1);
-    init_keycode(0x7B, 0x07, 1);
-    /* VK_PRINT is too big, handled separately */
-    init_keycode(0x91, 0x7E, 1);
-    /* VK_PAUSE is too big, handled separately */
-    init_keycode(0xDB, 0x54, 1);
-    init_keycode(0x2D, 0x70E0, 2);
-    init_keycode(0x24, 0x6CE0, 2);
-    init_keycode(0x21, 0x7DE0, 2);
-    init_keycode(0x2E, 0x71E0, 2);
-    init_keycode(0x23, 0x69E0, 2);
-    init_keycode(0x22, 0x7AE0, 2);
-    init_keycode(0x26, 0x75E0, 2);
-    init_keycode(0x25, 0x6BE0, 2);
-    init_keycode(0x28, 0x72E0, 2);
-    init_keycode(0x27, 0x74E0, 2);
-    init_keycode(0x90, 0x77, 1);
-    init_keycode(0x6F, 0x4AE0, 2);
-    init_keycode(0x6A, 0x7C, 1);
-    init_keycode(0x6D, 0x7B, 1);
-    init_keycode(0x6B, 0x79, 1);
-    // need to handle disabled numlock somehow...
-    //init_keycode(VK_RETURN, 0x5AE0, 2);
-    init_keycode(0x6E, 0x71, 1); //init_keycode(XK_KP_Delete, 0x71, 1);
-    init_keycode(0x60, 0x70, 1); //init_keycode(XK_KP_Insert, 0x70, 1);
-    init_keycode(0x61, 0x69, 1); //init_keycode(XK_KP_End, 0x69, 1);
-    init_keycode(0x62, 0x72, 1); //init_keycode(XK_KP_Down, 0x72, 1);
-    init_keycode(0x63, 0x7A, 1); //init_keycode(XK_KP_Page_Down, 0x7A, 1);
-    init_keycode(0x64, 0x6B, 1); //init_keycode(XK_KP_Left, 0x6B, 1);
-    init_keycode(0x65, 0x73, 1); //init_keycode(XK_KP_Begin, 0x73, 1);
-    init_keycode(0x66, 0x74, 1); //init_keycode(XK_KP_Right, 0x74, 1);
-    init_keycode(0x67, 0x6C, 1); //init_keycode(XK_KP_Home, 0x6C, 1);
-    init_keycode(0x68, 0x75, 1); //init_keycode(XK_KP_Up, 0x75, 1);
-    init_keycode(0x69, 0x7D, 1); //init_keycode(XK_KP_Page_Up, 0x7D, 1);
-    init_keycode(0xDD, 0x5B, 1);
-    init_keycode(0xBA, 0x4C, 1);
-    init_keycode(0xDE, 0x52, 1);
-    init_keycode(0xBC, 0x41, 1);
-    init_keycode(0xBE, 0x49, 1);
-    init_keycode(0xBF, 0x4A, 1);
+static hid_key_t win32_key_to_hid(uint32_t win32_key)
+{
+    if (win32_key < sizeof(win32_key_to_hid_byte_map)) {
+        return win32_key_to_hid_byte_map[win32_key];
+    }
+    return HID_KEY_NONE;
 }
 
-void fb_create_window(struct fb_data *data, unsigned width, unsigned height, const char* name)
+bool fb_window_create(fb_window_t* win)
 {
-    win32fb_data* wdata = safe_calloc(1, sizeof(win32fb_data));
-    data->winsys_data = (void*)wdata;
-    data->framebuffer = safe_calloc((size_t)width * height, 4);
-
-    wdata->hwnd = NULL;
-    wdata->x = width;
-    wdata->y = height;
-    strncpy(wdata->name, name, sizeof(wdata->name) - 1);
-    wdata->name[sizeof(wdata->name) - 1] = 0;
-
     if (winclass_atom == 0) {
-        WNDCLASSW wc = { 0 };
+        WNDCLASSW wc = {0};
         wc.lpfnWndProc   = WindowProc;
         wc.hInstance     = GetModuleHandle(NULL);
         wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
@@ -185,105 +168,114 @@ void fb_create_window(struct fb_data *data, unsigned width, unsigned height, con
         winclass_atom = RegisterClassW(&wc);
         if (winclass_atom == 0) {
             MessageBoxW(NULL, L"Failed to register window class!", L"RVVM Error", MB_OK | MB_ICONERROR);
-            return;
+            return false;
         }
 
         HWND tmp_window = CreateWindowW(
-            L"RVVM_window", L"RVVM",
+            L"RVVM_window", L"tmp",
             WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
             CW_USEDEFAULT, CW_USEDEFAULT, 128, 128,
             NULL, NULL, GetModuleHandle(NULL), NULL);
-        RECT rect;
-        GetClientRect(tmp_window, &rect);
-        winborder_x = 128 - (rect.right - rect.left);
-        winborder_y = 128 - (rect.bottom - rect.top);
-        DestroyWindow(tmp_window);
-
-        init_keycodes();
+        if (tmp_window) {
+            // Workaround to calculate client rectangle size
+            RECT rect;
+            GetClientRect(tmp_window, &rect);
+            if ((rect.right - rect.left) <= 128 && (rect.bottom - rect.top) <= 128) {
+                winborder_x = 128 - (rect.right - rect.left);
+                winborder_y = 128 - (rect.bottom - rect.top);
+            } else rvvm_warn("Invalid win32 client rectangle!");
+            DestroyWindow(tmp_window);
+        } else {
+            rvvm_error("CreateWindowW() failed");
+            return false;
+        }
     }
-    /*
-    * It's not possible to work with window from thread other than the window creator in win32,
-    * so the hacky workaround is to move window creation to fb_update...
-    */
+    
+    win->data = safe_calloc(sizeof(win_data_t), 1);
+    win->fb.format = RGB_FMT_A8R8G8B8;
+    win->fb.buffer = safe_calloc(framebuffer_size(&win->fb), 1);
+    
+    // It's not possible to work with window from thread other than the window creator in win32,
+    // so the hacky workaround is to move window creation to fb_update...
+    return true;
 }
 
-void fb_close_window(struct fb_data *data)
+void fb_window_close(fb_window_t* win)
 {
-    if (winclass_atom == 0) return;
-    win32fb_data* wdata = (win32fb_data*)data->winsys_data;
-    DestroyWindow(wdata->hwnd);
-    free(wdata);
+    DestroyWindow(win->data->hwnd);
+    free(win->data);
 }
 
-void fb_update(struct fb_data *data)
+void fb_window_update(fb_window_t* win)
 {
-    if (winclass_atom == 0) return;
-    win32fb_data* wdata = (win32fb_data*)data->winsys_data;
-
-    if (wdata->hwnd == NULL) {
-        wchar_t window_name[32] = {0};
-        MultiByteToWideChar(CP_UTF8, 0, wdata->name, strlen(wdata->name), window_name, 32);
-        wdata->hwnd = CreateWindowW(
-            L"RVVM_window", window_name,
+    if (win->data->hwnd == NULL) {
+        win->data->hwnd = CreateWindowW(
+            L"RVVM_window", L"RVVM",
             WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX | WS_VISIBLE,
-            CW_USEDEFAULT, CW_USEDEFAULT, wdata->x + winborder_x, wdata->y + winborder_y,
+            CW_USEDEFAULT, CW_USEDEFAULT, win->fb.width + winborder_x, win->fb.height + winborder_y,
             NULL, NULL, GetModuleHandle(NULL), NULL);
+        if (win->data->hwnd == NULL) return;
     }
 
-    InvalidateRect(wdata->hwnd, NULL, 1);
+    InvalidateRect(win->data->hwnd, NULL, 1);
     MSG Msg;
-    while (PeekMessage(&Msg, wdata->hwnd, 0, 0, PM_REMOVE)) {
+    while (PeekMessage(&Msg, win->data->hwnd, 0, 0, PM_REMOVE)) {
         switch (Msg.message) {
             case WM_MOUSEMOVE: {
                 POINTS cur = MAKEPOINTS(Msg.lParam);
-                ps2_handle_mouse(data->mouse, cur.x - wdata->lastcur.x, wdata->lastcur.y - cur.y, &wdata->btns);
-                wdata->lastcur = cur;
+                hid_mouse_place(win->mouse, cur.x, cur.y);
                 break;
             }
             case WM_KEYDOWN:
+            case WM_SYSKEYDOWN: // For handling F10
                 // Disable autorepeat keypresses
                 if ((Msg.lParam & KF_REPEAT) == 0) {
-                    struct key k = keysym2makecode(Msg.wParam);
-                    if (k.len) ps2_handle_keyboard(data->keyboard, &k, true);
+                    hid_keyboard_press(win->keyboard, win32_key_to_hid(Msg.wParam));
                 }
                 break;
-            case WM_KEYUP: {
-                    struct key k = keysym2makecode(Msg.wParam);
-                    if (k.len) ps2_handle_keyboard(data->keyboard, &k, false);
+            case WM_KEYUP:
+            case WM_SYSKEYUP: // For handling F10
+                if ((Msg.lParam & KF_REPEAT) == 0) {
+                    hid_keyboard_release(win->keyboard, win32_key_to_hid(Msg.wParam));
                 }
                 break;
             case WM_LBUTTONDOWN:
-                wdata->btns.left = true;
-                ps2_handle_mouse(data->mouse, 0, 0, &wdata->btns);
+                hid_mouse_press(win->mouse, HID_BTN_LEFT);
                 break;
             case WM_LBUTTONUP:
-                wdata->btns.left = false;
-                ps2_handle_mouse(data->mouse, 0, 0, &wdata->btns);
+                hid_mouse_release(win->mouse, HID_BTN_LEFT);
                 break;
             case WM_RBUTTONDOWN:
-                wdata->btns.right = true;
-                ps2_handle_mouse(data->mouse, 0, 0, &wdata->btns);
+                hid_mouse_press(win->mouse, HID_BTN_RIGHT);
                 break;
             case WM_RBUTTONUP:
-                wdata->btns.right = false;
-                ps2_handle_mouse(data->mouse, 0, 0, &wdata->btns);
+                hid_mouse_release(win->mouse, HID_BTN_RIGHT);
+                break;
+            case WM_MBUTTONDOWN:
+                hid_mouse_press(win->mouse, HID_BTN_MIDDLE);
+                break;
+            case WM_MBUTTONUP:
+                hid_mouse_release(win->mouse, HID_BTN_MIDDLE);
+                break;
+            case WM_MOUSEWHEEL:
+                hid_mouse_scroll(win->mouse, -GET_WHEEL_DELTA_WPARAM(Msg.wParam) / WHEEL_DELTA);
                 break;
             case WM_PAINT: {
                 PAINTSTRUCT ps;
                 RECT rect;
-                HBITMAP hBitmap = CreateBitmap(wdata->x, wdata->y, 1, 32, data->framebuffer);
-                HDC hdc = GetDC(wdata->hwnd);
+                HBITMAP hBitmap = CreateBitmap(win->fb.width, win->fb.height, 1, 32, win->fb.buffer);
+                HDC hdc = GetDC(win->data->hwnd);
                 HDC hdcc = CreateCompatibleDC(hdc);
-                ReleaseDC(wdata->hwnd, hdc);
+                ReleaseDC(win->data->hwnd, hdc);
                 SelectObject(hdcc, hBitmap);
-                hdc = BeginPaint(wdata->hwnd, &ps);
-                GetClientRect(wdata->hwnd, &rect);
+                hdc = BeginPaint(win->data->hwnd, &ps);
+                GetClientRect(win->data->hwnd, &rect);
 #ifndef UNDER_CE
                 SetStretchBltMode(hdc, STRETCH_HALFTONE);
 #endif
-                StretchBlt(hdc, 0, 0, rect.right, rect.bottom, hdcc, 0, 0, wdata->x, wdata->y, SRCCOPY);
+                StretchBlt(hdc, 0, 0, rect.right, rect.bottom, hdcc, 0, 0, win->fb.width, win->fb.height, SRCCOPY);
 
-                EndPaint(wdata->hwnd, &ps);
+                EndPaint(win->data->hwnd, &ps);
 
                 DeleteObject(hBitmap);
                 DeleteDC(hdcc);
