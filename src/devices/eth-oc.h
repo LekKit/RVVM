3a5a3cf094ee6f034deafc0c80646f24744edb37
LekKit <50500857+LekKit@users.noreply.github.com>
2022-11-26 10:05:20 +0200

    Simplify ethoc_init_auto()

--- src/devices/eth-oc.h
+++ src/devices/eth-oc.h
@@ -1,6 +1,7 @@
 /*
 eth-oc.h - OpenCores Ethernet MAC controller
 Copyright (C) 2021  cerg2010cerg2010 <github.com/cerg2010cerg2010>
+                    LekKit <github.com/LekKit>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -25,6 +26,6 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #define ETHOC_DEFAULT_MMIO 0x21000000
 
 PUBLIC void ethoc_init(rvvm_machine_t* machine, rvvm_addr_t base_addr, plic_ctx_t* plic, uint32_t irq);
-PUBLIC void ethoc_init_auto(rvvm_machine_t* machine, plic_ctx_t* plic);
+PUBLIC void ethoc_init_auto(rvvm_machine_t* machine);
 
 #endif
