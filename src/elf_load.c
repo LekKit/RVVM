218d80a74f2d93c8fc15ee1e581d8b22ad0db96f
LekKit <50500857+LekKit@users.noreply.github.com>
2023-03-28 16:33:00 +0300

    elf_load: Complete rewrite
    - Fully self-contained, no dependency upon <elf.h>
    - Support both ELFCLASS32 and ELFCLASS64
    - Support objcopy-like functionality
    - WIP userland ELF loader, extended API
    - Fix endianness issues

--- src/elf_load.c
+++ src/elf_load.c
@@ -1,6 +1,7 @@
 /*
-elf_load.c - ELF loader
-Copyright (C) 2021  cerg2010cerg2010 <github.com/cerg2010cerg2010>
+elf_load.c - ELF Loader
+Copyright (C) 2023  LekKit <github.com/LekKit>
+              2021  cerg2010cerg2010 <github.com/cerg2010cerg2010>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -16,246 +17,118 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-// Currently broken
-#if 0
-
 #include "elf_load.h"
-#include <stddef.h>
-
-#if !defined _WIN32 && !defined __APPLE__
-
-#include <stdio.h>
-#include <string.h>
-
-#include "bit_ops.h"
-#include "riscv32.h"
 #include "mem_ops.h"
-#include "riscv32_csr.h"
-#include "riscv32_mmu.h"
+#include "utils.h"
 
-#include <elf.h>
+#define ELF_ET_NONE 0x0
+#define ELF_ET_REL  0x1
+#define ELF_ET_EXEC 0x2
+#define ELF_ET_DYN  0x3
 
-#ifndef PN_XNUM
-#define PN_XNUM 0xffff
-#endif
+#define ELF_PT_NULL    0x0
+#define ELF_PT_LOAD    0x1
+#define ELF_PT_DYNAMIC 0x2
+#define ELF_PT_INTERP  0x3
+#define ELF_PT_NOTE    0x4
+#define ELF_PT_SHLIB   0x5
+#define ELF_PT_PHDR    0x6
+#define ELF_PT_TLS     0x7
 
-bool riscv32_elf_load_by_path(rvvm_hart_t *vm, const char *path, bool use_mmu, ptrdiff_t offset)
+#define ELF_PF_X 0x1
+#define ELF_PF_W 0x2
+#define ELF_PF_R 0x4
+
+// TODO: Handling >64k PHENTs
+#define ELF_PN_XNUM 0xFFFF
+
+#define WRAP_ERR(cond, error) \
+    if (!(cond)) { \
+        rvvm_error(error); \
+        return false; \
+    }
+
+bool elf_load_file(rvfile_t* file, elf_desc_t* elf)
 {
-	FILE *fp = fopen(path, "rb");
-	if (!fp)
-	{
-		printf("Unable to open ELF file %s\n", path);
-		return false;
-	}
+    uint8_t tmp[64];
+    WRAP_ERR(file && elf, "Invalid arguments to elf_load_file()");
+    WRAP_ERR(rvread(file, tmp, 64, 0) == 64, "Failed to read ELF header");
+    WRAP_ERR(read_uint32_le_m(tmp) == 0x464c457F, "Not an ELF file");
+    WRAP_ERR(tmp[4] == 1 || tmp[4] == 2, "Invalid ELF class");
+    WRAP_ERR(tmp[5] == 1, "Not a little-endian ELF");
 
-	bool status = true;
-	Elf32_Ehdr ehdr;
-	if (1 != fread(&ehdr, sizeof(ehdr), 1, fp))
-	{
-		printf("Unable to read ELF file header of %s\n", path);
-		status = false;
-		goto err_fclose;
+    // Parse ELF header
+    bool objcopy = !!elf->base;
+    bool class64 = (tmp[4] == 2);
+    uint64_t elf_entry = class64 ? read_uint64_le_m(tmp + 24) : read_uint32_le_m(tmp + 24);
+    uint64_t elf_phoff = class64 ? read_uint64_le_m(tmp + 32) : read_uint32_le_m(tmp + 28);
+    //uint64_t elf_shoff = class64 ? read_uint64_le_m(tmp + 40) : read_uint32_le_m(tmp + 32);
+    size_t   elf_phnsz = class64 ? 56 : 32;
+    size_t   elf_phnum = read_uint16_le_m(tmp + (class64 ? 56 : 44));
 
-	}
+    elf->entry = elf_entry;
+    elf->interp_path = NULL;
+    elf->phdr = 0;
+    elf->phnum = elf_phnum;
 
-	size_t phdr_num = ehdr.e_phnum;
-	if (phdr_num == PN_XNUM)
-	{
-		if (ehdr.e_shnum <= 0)
-		{
-			printf("Unable to load ELF section header of %s - section count is 0\n", path);
-			status = false;
-			goto err_fclose;
-		}
+    // Determine lowest virtual address for objcopy
+    uint64_t objcopy_base = -1;
+    if (objcopy) for (size_t i=0; i<elf_phnum; ++i) {
+        uint64_t elf_phent_off = elf_phoff + (elf_phnsz * i);
+        WRAP_ERR(rvread(file, tmp, elf_phnsz, elf_phent_off) == elf_phnsz, "Failed to read ELF phent");
+        uint64_t p_vaddr = class64 ? read_uint64_le_m(tmp + 16) : read_uint32_le_m(tmp + 8);
+        if (read_uint32_le_m(tmp) == ELF_PT_LOAD && p_vaddr < objcopy_base) objcopy_base = p_vaddr;
+    }
 
-		if (fseek(fp, ehdr.e_shoff, SEEK_SET))
-		{
-			printf("Unable to load ELF section header of %s - unable to seek to section offset\n", path);
-			status = false;
-			goto err_fclose;
-		}
+    for (size_t i=0; i<elf_phnum; ++i) {
+        uint64_t elf_phent_off = elf_phoff + (elf_phnsz * i);
+        WRAP_ERR(rvread(file, tmp, elf_phnsz, elf_phent_off) == elf_phnsz, "Failed to read ELF phent");
+        uint32_t p_type = read_uint32_le_m(tmp);
+        uint64_t p_offset = class64 ? read_uint64_le_m(tmp + 8) : read_uint32_le_m(tmp + 4);
+        uint64_t p_vaddr = class64 ? read_uint64_le_m(tmp + 16) : read_uint32_le_m(tmp + 8);
+        uint64_t p_fsize = class64 ? read_uint64_le_m(tmp + 32) : read_uint32_le_m(tmp + 16);
+        uint64_t p_memsz = class64 ? read_uint64_le_m(tmp + 40) : read_uint32_le_m(tmp + 20);
+        uint32_t p_flags = class64 ? read_uint32_le_m(tmp + 4) : read_uint32_le_m(tmp + 24);
 
-		Elf32_Shdr shdr;
-		if (1 != fread(&shdr, sizeof(shdr), 1, fp))
-		{
-			printf("Unable to load ELF section header of %s\n", path);
-			status = false;
-			goto err_fclose;
-		}
+        if (p_type == ELF_PT_LOAD || p_type == ELF_PT_PHDR) {
+            // Load ELF program segment or PHDR segment
+            if (objcopy) {
+                p_vaddr -= objcopy_base;
+                WRAP_ERR(p_vaddr + p_memsz <= elf->buf_size, "ELF does not fit in objcopy buffer");
+            } else {
+                // TODO: Userland ELF mapping
+                void*  va_addr = ((uint8_t*)elf->base) + (p_vaddr & ~0xFFFULL);
+                size_t va_size = (p_memsz + (p_vaddr & 0xFFFULL) + 0xFFFULL) & ~0xFFFULL;
+                // mmap(va_addr, va_size, mmap_flags(p_flags), MAP_PRIVATE | MAP_ANON, -1, 0);
+                UNUSED(va_addr);
+                UNUSED(va_size);
+                UNUSED(p_flags);
+            }
 
-		phdr_num = shdr.sh_info;
-	}
+            void* vaddr = ((uint8_t*)elf->base) + p_vaddr;
+            WRAP_ERR(rvread(file, vaddr, p_fsize, p_offset) == p_fsize, "Failed to read ELF segment");
+            if (p_type == ELF_PT_PHDR) elf->phdr = p_vaddr;
+        }
+        if (p_type == ELF_PT_INTERP && !objcopy && !elf->interp_path) {
+            // Get ELF interpreter path
+            elf->interp_path = safe_new_arr(char, p_fsize + 1);
+            WRAP_ERR(rvread(file, elf->interp_path, p_fsize, p_offset) == p_offset, "Failed to read ELF interp_path");
+        }
+    }
 
-	size_t pgd = 0;
-	size_t load_addr = offset;
-	if (use_mmu)
-	{
-		/* set up page table */
-		/* XXX: need to restore this on error exit? */
-
-		/* put memory mappings at the end of physical memory to avoid overlap with program */
-		pgd = vm->mem.begin + vm->mem.size - 4096;
-		uint32_t satp = (pgd >> 12) | (1 << 31);
-		riscv32_csr_op(vm, 0x180, &satp, CSR_SWAP);
-	}
-
-	for (size_t phdr_start = ehdr.e_phoff;
-			phdr_start < ehdr.e_phoff + phdr_num * ehdr.e_phentsize;
-			phdr_start += ehdr.e_phentsize)
-	{
-		if (fseek(fp, phdr_start, SEEK_SET))
-		{
-			printf("Unable to load ELF program header of %s - unable to seek to header offset 0x%zx\n", path, phdr_start);
-			status = false;
-			goto err_fclose;
-		}
-
-		Elf32_Phdr phdr;
-		if (1 != fread(&phdr, sizeof(phdr), 1, fp))
-		{
-			printf("Unable to load ELF program header of %s - unable to read header offset 0x%zx\n", path, phdr_start);
-			status = false;
-			goto err_fclose;
-		}
-
-		if (phdr.p_type != PT_LOAD)
-		{
-			continue;
-		}
-
-		size_t dest_addr = load_addr + phdr.p_vaddr;
-		if (dest_addr < vm->mem.begin || dest_addr + phdr.p_memsz >= vm->mem.begin + vm->mem.size)
-		{
-			printf("Unable to load ELF segment at offset 0x%zx of file %s - segment doesnt't fit in memory\n", phdr_start, path);
-			printf("load addr: 0x%zx p_memsz: 0x%x end: 0x%zx\n", dest_addr, phdr.p_memsz, dest_addr + phdr.p_memsz);
-			printf("mem begin: 0x%"PRIxXLEN" size: 0x%"PRIxXLEN" end: 0x%"PRIxXLEN"\n", vm->mem.begin, vm->mem.size, vm->mem.begin + vm->mem.size);
-			status = false;
-			goto err_fclose;
-		}
-
-		if (fseek(fp, phdr.p_offset, SEEK_SET))
-		{
-			printf("Unable to load ELF segment at offset 0x%zx of file %s - unable to seek to start of the segment\n", phdr_start, path);
-			status = false;
-			goto err_fclose;
-		}
-
-		vmptr_t temp_mem = malloc(phdr.p_filesz);
-		if (temp_mem == NULL) {
-			printf("Unable to allocate memory of size %d\n", phdr.p_filesz);
-			status = false;
-			goto err_fclose;
-		}
-
-		if (1 != fread(temp_mem, phdr.p_filesz, 1, fp))
-		{
-			printf("Unable to read ELF segment at offset 0x%zx of file %s\n", phdr_start, path);
-			status = false;
-			goto err_fclose;
-		}
-
-		riscv32_physmem_op(vm, dest_addr, temp_mem, phdr.p_filesz, MMU_WRITE);
-		free(temp_mem);
-
-		/* fill .bss */
-		temp_mem = calloc(1, phdr.p_memsz - phdr.p_filesz);
-		if (temp_mem == NULL) {
-			printf("Unable to allocate memory of size %d\n", phdr.p_memsz - phdr.p_filesz);
-			status = false;
-			goto err_fclose;
-		}
-
-		riscv32_physmem_op(vm, dest_addr + phdr.p_filesz, temp_mem, phdr.p_memsz - phdr.p_filesz, MMU_WRITE);
-		free(temp_mem);
-
-		if (!use_mmu)
-		{
-			/* adjust entry point offset */
-			if (ehdr.e_entry >= phdr.p_vaddr && ehdr.e_entry < phdr.p_vaddr + phdr.p_memsz)
-			{
-				ehdr.e_entry = ehdr.e_entry + load_addr;
-			}
-
-			continue;
-		}
-
-		/* fill page table */
-		size_t segment_start_addr = dest_addr;
-
-		size_t pte1_start = vm->root_page_table + GET_VPN1(phdr.p_vaddr) * 4;
-		size_t pte1_count = GET_VPN1(phdr.p_memsz + bit_mask(22));//(page_count + gen_mask(10)) >> 10;
-		size_t page_count = (phdr.p_memsz + bit_mask(12)) >> 12;
-
-		size_t pte = MMU_VALID_PTE
-			| MMU_PAGE_ACCESSED | MMU_PAGE_DIRTY /* optimize */
-			| MMU_READ * !!(phdr.p_flags & PF_R)
-			| MMU_WRITE * !!(phdr.p_flags & PF_W)
-			| MMU_EXEC * !!(phdr.p_flags & PF_X);
-
-		for (size_t i = 0; i < pte1_count; ++i)
-		{
-			size_t pte1_addr = pte1_start + i * 4;
-			uint32_t pte1;
-			riscv32_physmem_op(vm, pte1_addr, (vmptr_t)&pte1, sizeof(pte1), MMU_READ);
-			pte1 = read_uint32_le(&pte1);
-
-			size_t pte0_start;
-			if (pte1 & MMU_VALID_PTE)
-			{
-				pte0_start = GET_PHYS_ADDR(pte1);
-			}
-			else
-			{
-				pgd -= 4096;
-				pte0_start = pgd;
-				assert((pte0_start & bit_mask(12)) == 0);
-
-				uint32_t data = SET_PHYS_ADDR(MMU_VALID_PTE, pte0_start);
-				riscv32_physmem_op(vm, pte1_addr, (vmptr_t)&data, sizeof(data), MMU_WRITE);
-			}
-			pte0_start += GET_VPN2(phdr.p_vaddr) * 4;
-
-			size_t pte0_count = page_count > (1 << 10) ? (1 << 10) : page_count;
-
-			for (size_t j = 0; j < pte0_count; ++j)
-			{
-				size_t pte0_addr = pte0_start + j * 4;
-
-				uint32_t data = SET_PHYS_ADDR(pte, segment_start_addr + j * 4096);
-				riscv32_physmem_op(vm, pte0_addr, (vmptr_t)&data, sizeof(data), MMU_WRITE);
-			}
-
-			page_count -= pte0_count;
-			segment_start_addr += pte0_count * 4096;
-		}
-
-		assert(page_count == 0);
-	}
-
-	vm->registers[REGISTER_PC] = ehdr.e_entry;
-
-#ifdef RV_DEBUG
-	if (use_mmu)
-	{
-		riscv32_mmu_dump(vm);
-	}
-#endif
-
-err_fclose:
-	fclose(fp);
-	return status;
+    return true;
 }
 
-#else
-
-bool riscv32_elf_load_by_path(rvvm_hart_t *vm, const char *path, bool use_mmu, ptrdiff_t offset)
+bool bin_objcopy(rvfile_t* file, void* buffer, size_t size, bool try_elf)
 {
-    UNUSED(vm);
-    UNUSED(path);
-    UNUSED(use_mmu);
-    UNUSED(offset);
-    return false;
+    uint8_t mag[4] = {0};
+    if (try_elf && rvread(file, mag, 4, 0) == 4 && read_uint32_le_m(mag) == 0x464c457F) {
+        elf_desc_t elf = {
+            .base = buffer,
+            .buf_size = size,
+        };
+        if (elf_load_file(file, &elf)) return true;
+    }
+    return rvread(file, buffer, size, 0);
 }
 
-#endif
-#endif
--- src/elf_load.h
+++ src/elf_load.h
@@ -1,6 +1,7 @@
 /*
-elf_load.h - ELF loader
-Copyright (C) 2021  cerg2010cerg2010 <github.com/cerg2010cerg2010>
+elf_load.h - ELF Loader
+Copyright (C) 2023  LekKit <github.com/LekKit>
+              2021  cerg2010cerg2010 <github.com/cerg2010cerg2010>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -16,8 +17,27 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-#pragma once
+#ifndef ELF_LOAD_H
+#define ELF_LOAD_H
 
-#include "riscv32.h"
+#include "blk_io.h"
 
-bool riscv32_elf_load_by_path(rvvm_hart_t *vm, const char *path, bool use_mmu, ptrdiff_t offset);
+typedef struct {
+    // Pass a buffer for objcopy, NULL for userland loading
+    // Receive base ELF address for userland
+    void*  base;
+    // Objcopy buffer size
+    size_t buf_size;
+
+    // Various loaded ELF info
+    size_t entry;
+    char*  interp_path;
+    size_t phdr;
+    size_t phnum;
+} elf_desc_t;
+
+bool elf_load_file(rvfile_t* file, elf_desc_t* elf);
+
+bool bin_objcopy(rvfile_t* file, void* buffer, size_t size, bool allow_elf);
+
+#endif
