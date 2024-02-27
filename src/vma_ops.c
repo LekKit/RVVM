c8d3a58ba2aa4aa7ab3959e60979bac75f1487b7
LekKit <50500857+LekKit@users.noreply.github.com>
2024-02-27 14:39:45 +0200

    vma_ops: Only use MADV_DONTNEED on Linux
    - MADV_DONTNEED semantics differ on non-Linux kernels, as in they don't free pages

--- src/vma_ops.c
+++ src/vma_ops.c
@@ -353,7 +353,7 @@ bool vma_clean(void* addr, size_t size, bool lazy)
 #ifdef MADV_FREE
     if (lazy) return madvise(addr, size, MADV_FREE) == 0;
 #endif
-#ifdef MADV_DONTNEED
+#if defined(__linux__) && defined(MADV_DONTNEED)
     return madvise(addr, size, MADV_DONTNEED) == 0;
 #endif
 #endif
