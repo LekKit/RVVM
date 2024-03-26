c41d875edef1895c828e5b97a07b4cbdd6d79d86
LekKit <50500857+LekKit@users.noreply.github.com>
2024-03-26 20:32:30 +0200

    rvtimer: Don't use coarse clock on Emscripten

--- src/rvtimer.c
+++ src/rvtimer.c
@@ -58,9 +58,9 @@ uint64_t rvtimer_clocksource(uint64_t freq)
 #elif defined(CLOCK_REALTIME) || defined(CLOCK_MONOTONIC)
 // Use POSIX clock_gettime(), with a fast monotonic clock if possible
 #include <unistd.h>
-#if defined(CLOCK_MONOTONIC_FAST)
+#if defined(CLOCK_MONOTONIC_FAST) && !defined(__EMSCRIPTEN__)
 #define CHOSEN_POSIX_CLOCK CLOCK_MONOTONIC_FAST
-#elif defined(CLOCK_MONOTONIC_COARSE)
+#elif defined(CLOCK_MONOTONIC_COARSE) && !defined(__EMSCRIPTEN__)
 #define CHOSEN_POSIX_CLOCK CLOCK_MONOTONIC_COARSE
 #elif defined(CLOCK_MONOTONIC_RAW)
 #define CHOSEN_POSIX_CLOCK CLOCK_MONOTONIC_RAW
