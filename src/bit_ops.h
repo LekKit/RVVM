dc82ce0c5d787ce4a6bc39b01bb44a52b49644a5
LekKit <50500857+LekKit@users.noreply.github.com>
2024-04-04 14:16:33 +0300

    bit_ops: Optimize bit_popcnt fallback
    - Use popcnt_2 algorithm, ~6 insn less on most arches

--- src/bit_ops.h
+++ src/bit_ops.h
@@ -179,12 +179,11 @@ static inline bitcnt_t bit_popcnt32(uint32_t val)
 #if GNU_BUILTIN(__builtin_popcount)
     return __builtin_popcount(val);
 #else
-    val = (val & 0x55555555) + ((val >>  1) & 0x55555555);
-    val = (val & 0x33333333) + ((val >>  2) & 0x33333333);
-    val = (val & 0x0F0F0F0F) + ((val >>  4) & 0x0F0F0F0F);
-    val = (val & 0x00FF00FF) + ((val >>  8) & 0x00FF00FF);
-    val = (val & 0x0000FFFF) + ((val >> 16) & 0x0000FFFF);
-    return val;
+    val -= (val >> 1) & 0x55555555;
+    val = (val & 0x33333333) + ((val >> 2) & 0x33333333);
+    val = (val + (val >> 4)) & 0x0F0F0F0F;
+    val += val >>  8;
+    return (val + (val >> 16)) & 0x3F;
 #endif
 }
 
