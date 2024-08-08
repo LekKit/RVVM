9419cc39c78402d0025b34fa97935e172ab298b1
LekKit <50500857+LekKit@users.noreply.github.com>
2024-08-08 18:04:07 +0300

    riscv_hart: Refactoring, better documentation
    - Do not expose riscv_handle_irqs() outside riscv_hart implementation
    - Improve/update documentation
    - Small refactoring

--- src/riscv_hart.c
+++ src/riscv_hart.c
@@ -26,6 +26,14 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include "atomics.h"
 #include "bit_ops.h"
 
+// Valid vm->wait_event values
+#define HART_STOPPED 0
+#define HART_RUNNING 1
+
+// Valid vm->pending_events bits deliverable to the hart
+#define HART_EVENT_PAUSE   0x1 // Pause the hart in a consistent state
+#define HART_EVENT_PREEMPT 0x2 // Preempt the hart for vm->preempt_ms
+
 rvvm_hart_t* riscv_hart_init(rvvm_machine_t* machine)
 {
     rvvm_hart_t* vm = safe_new_obj(rvvm_hart_t);
@@ -55,6 +63,27 @@ rvvm_hart_t* riscv_hart_init(rvvm_machine_t* machine)
     return vm;
 }
 
+void riscv_hart_prepare(rvvm_hart_t *vm)
+{
+#ifdef USE_JIT
+    if (!vm->jit_enabled && rvvm_get_opt(vm->machine, RVVM_OPT_JIT)) {
+        vm->jit_enabled = rvjit_ctx_init(&vm->jit, rvvm_get_opt(vm->machine, RVVM_OPT_JIT_CACHE));
+
+        if (vm->jit_enabled) {
+            rvjit_set_rv64(&vm->jit, vm->rv64);
+            if (!rvvm_get_opt(vm->machine, RVVM_OPT_JIT_HARVARD)) {
+                rvjit_init_memtracking(&vm->jit, vm->mem.size);
+            }
+        } else {
+            rvvm_set_opt(vm->machine, RVVM_OPT_JIT, false);
+            rvvm_warn("RVJIT failed to initialize, falling back to interpreter");
+        }
+    }
+#else
+    UNUSED(vm);
+#endif
+}
+
 void riscv_hart_free(rvvm_hart_t* vm)
 {
 #ifdef USE_JIT
@@ -64,45 +93,9 @@ void riscv_hart_free(rvvm_hart_t* vm)
     free(vm);
 }
 
-void riscv_hart_run(rvvm_hart_t* vm)
-{
-    rvvm_info("Hart %p started", vm);
-
-    while (true) {
-        // Allow hart to run
-        atomic_store_uint32_ex(&vm->wait_event, HART_RUNNING, ATOMIC_RELAXED);
-
-        // Handle events
-        vm->csr.ip |= atomic_swap_uint32(&vm->pending_irqs, 0);
-        uint32_t events = atomic_swap_uint32(&vm->pending_events, 0);
-
-        if ((vm->csr.ip & (1U << INTERRUPT_MTIMER)) && !rvtimer_pending(&vm->timer)) {
-            riscv_interrupt_clear(vm, INTERRUPT_MTIMER);
-        }
-
-        if (unlikely(events)) {
-            if (events & EXT_EVENT_PAUSE) {
-                rvvm_info("Hart %p stopped", vm);
-                return;
-            } else if (events & EXT_EVENT_PREEMPT) {
-                sleep_ms(atomic_swap_uint32(&vm->preempt_ms, 0));
-            }
-        }
-
-        riscv_handle_irqs(vm, false);
-
-        // Run the hart
-        riscv_run_till_event(vm);
-        if (vm->trap) {
-            vm->registers[REGISTER_PC] = vm->trap_pc;
-            vm->trap = false;
-        }
-    }
-}
-
 rvvm_addr_t riscv_hart_run_userland(rvvm_hart_t* vm)
 {
-    vm->user_traps = true;
+    vm->userland = true;
     atomic_store_uint32(&vm->wait_event, HART_RUNNING);
     riscv_run_till_event(vm);
     if (vm->trap) {
@@ -112,9 +105,20 @@ rvvm_addr_t riscv_hart_run_userland(rvvm_hart_t* vm)
     return vm->csr.cause[PRIVILEGE_USER];
 }
 
-#ifdef USE_RV64
+void riscv_switch_priv(rvvm_hart_t* vm, uint8_t priv_mode)
+{
+    // true if one is S/U, other is M/H
+    bool mmu_toggle = (vm->priv_mode & 2) != (priv_mode & 2);
+    vm->priv_mode = priv_mode;
+    riscv_update_xlen(vm);
+
+    // May unwind to dispatch
+    if (mmu_toggle) riscv_tlb_flush(vm);
+}
+
 void riscv_update_xlen(rvvm_hart_t* vm)
 {
+#ifdef USE_RV64
     bool rv64 = false;
     switch (vm->priv_mode) {
         case PRIVILEGE_MACHINE:
@@ -139,20 +143,9 @@ void riscv_update_xlen(rvvm_hart_t* vm)
 #endif
         riscv_restart_dispatch(vm);
     }
-}
+#else
+    UNUSED(vm);
 #endif
-
-void riscv_switch_priv(rvvm_hart_t* vm, uint8_t priv_mode)
-{
-    // true if one is S/U, other is M/H
-    bool mmu_toggle = (vm->priv_mode & 2) != (priv_mode & 2);
-    vm->priv_mode = priv_mode;
-#ifdef USE_RV64
-    riscv_update_xlen(vm);
-#endif
-
-    // May unwind to dispatch
-    if (mmu_toggle) riscv_tlb_flush(vm);
 }
 
 // Save current priv to xPP, xIE to xPIE, disable interrupts for target priv
@@ -187,8 +180,8 @@ void riscv_trap(rvvm_hart_t* vm, bitcnt_t cause, maxlen_t tval)
     if (cause < TRAP_ENVCALL_UMODE || cause > TRAP_ENVCALL_MMODE) {
         riscv_jit_discard(vm);
     }
-    if (vm->user_traps) {
-        // Defer usermode trap
+    if (vm->userland) {
+        // Defer userland trap
         vm->csr.cause[PRIVILEGE_USER] = cause;
         vm->csr.tval[PRIVILEGE_USER] = tval;
         vm->trap_pc = vm->registers[REGISTER_PC];
@@ -210,108 +203,14 @@ void riscv_trap(rvvm_hart_t* vm, bitcnt_t cause, maxlen_t tval)
     riscv_restart_dispatch(vm);
 }
 
-static inline maxlen_t riscv_cause_irq_mask(rvvm_hart_t* vm)
-{
-#ifdef USE_RV64
-    if (vm->rv64) {
-        return 0x8000000000000000ULL;
-    } else {
-        return 0x80000000U;
-    }
-#else
-    UNUSED(vm);
-    return 0x80000000U;
-#endif
-}
-
-bool riscv_handle_irqs(rvvm_hart_t* vm, bool wfi)
-{
-    // IRQs that are pending & enabled by mie
-    uint32_t pending_irqs = vm->csr.ip & vm->csr.ie;
-    if (unlikely(pending_irqs)) {
-        // Target privilege mode
-        uint8_t priv = PRIVILEGE_MACHINE;
-        uint32_t irqs = 0;
-        // Delegate pending IRQs from M to the highest possible mode
-        do {
-            irqs = pending_irqs & ~vm->csr.ideleg[priv];
-            pending_irqs &= vm->csr.ideleg[priv];
-            if (irqs) break;
-        } while (--priv);
-        // Skip if target priv < current priv, or equal and IRQs are disabled in status CSR
-        if (vm->priv_mode > priv) return false;
-        if (vm->priv_mode == priv && !((1 << vm->priv_mode) & vm->csr.status)) return false;
-
-        for (int i=11; i>=0; --i) {
-            if (irqs & (1 << i)) {
-                // Modify exception stack in csr.status
-                riscv_trap_priv_helper(vm, priv);
-                // Discard unfinished JIT block
-                riscv_jit_discard(vm);
-                // Switch privilege
-                riscv_switch_priv(vm, priv);
-                // Write exception info
-                vm->csr.epc[priv] = vm->registers[REGISTER_PC];
-                if (wfi) vm->csr.epc[priv] += 4;
-                vm->csr.cause[priv] = i | riscv_cause_irq_mask(vm);
-                vm->csr.tval[priv] = 0;
-                // Jump to trap vector
-                if (vm->csr.tvec[priv] & 1) {
-                    vm->registers[REGISTER_PC] = (vm->csr.tvec[priv] & (~3ULL)) + (i << 2);
-                } else {
-                    vm->registers[REGISTER_PC] = vm->csr.tvec[priv] & (~3ULL);
-                }
-                return true;
-            }
-        }
-    }
-    return false;
-}
-
 void riscv_restart_dispatch(rvvm_hart_t* vm)
 {
     atomic_store_uint32_ex(&vm->wait_event, HART_STOPPED, ATOMIC_RELAXED);
 }
 
-static void* riscv_hart_run_wrap(void* ptr)
-{
-    if (rvvm_getarg_int("noisolation") < 1) {
-        rvvm_restrict_this_thread();
-    }
-    riscv_hart_run((rvvm_hart_t*)ptr);
-    return NULL;
-}
-
-void riscv_hart_prepare(rvvm_hart_t *vm)
-{
-#ifdef USE_JIT
-    if (!vm->jit_enabled && rvvm_get_opt(vm->machine, RVVM_OPT_JIT)) {
-        vm->jit_enabled = rvjit_ctx_init(&vm->jit, rvvm_get_opt(vm->machine, RVVM_OPT_JIT_CACHE));
-
-        if (vm->jit_enabled) {
-            rvjit_set_rv64(&vm->jit, vm->rv64);
-            if (!rvvm_get_opt(vm->machine, RVVM_OPT_JIT_HARVARD)) {
-                rvjit_init_memtracking(&vm->jit, vm->mem.size);
-            }
-        } else {
-            rvvm_set_opt(vm->machine, RVVM_OPT_JIT, false);
-            rvvm_warn("RVJIT failed to initialize, falling back to interpreter");
-        }
-    }
-#else
-    UNUSED(vm);
-#endif
-}
-
-void riscv_hart_spawn(rvvm_hart_t *vm)
-{
-    atomic_store_uint32(&vm->pending_events, 0);
-    vm->thread = thread_create(riscv_hart_run_wrap, (void*)vm);
-}
-
 static void riscv_hart_notify(rvvm_hart_t* vm)
 {
-    atomic_store_uint32(&vm->wait_event, HART_STOPPED);
+    riscv_restart_dispatch(vm);
     // Wake from WFI sleep
     condvar_wake(vm->wfi_cond);
 }
@@ -332,22 +231,137 @@ void riscv_interrupt_clear(rvvm_hart_t* vm, bitcnt_t irq)
 #else
     atomic_and_uint32(&vm->csr.ip, ~(1U << irq));
 #endif
-    //rvvm_info("Hart %p сleared irq %d\n", vm, irq);
 }
 
 void riscv_hart_check_timer(rvvm_hart_t* vm)
 {
     // The hart thread checks if the timer is actually pending
     atomic_or_uint32(&vm->pending_irqs, 1U << INTERRUPT_MTIMER);
-    atomic_store_uint32(&vm->wait_event, HART_STOPPED);
+    riscv_restart_dispatch(vm);
 }
 
 void riscv_hart_preempt(rvvm_hart_t* vm, uint32_t preempt_ms)
 {
-    if (!preempt_ms) return;
-    atomic_store_uint32(&vm->preempt_ms, preempt_ms);
-    atomic_or_uint32(&vm->pending_events, EXT_EVENT_PREEMPT);
-    atomic_store_uint32(&vm->wait_event, HART_STOPPED);
+    if (preempt_ms) {
+        atomic_store_uint32(&vm->preempt_ms, preempt_ms);
+        atomic_or_uint32(&vm->pending_events, HART_EVENT_PREEMPT);
+        riscv_restart_dispatch(vm);
+    }
+}
+
+static inline maxlen_t riscv_cause_irq_mask(rvvm_hart_t* vm)
+{
+#ifdef USE_RV64
+    if (vm->rv64) {
+        return 0x8000000000000000ULL;
+    } else {
+        return 0x80000000U;
+    }
+#else
+    UNUSED(vm);
+    return 0x80000000U;
+#endif
+}
+
+static void riscv_handle_irqs(rvvm_hart_t* vm)
+{
+    // IRQs that are pending & enabled by mie
+    uint32_t pending_irqs = vm->csr.ip & vm->csr.ie;
+    if (unlikely(pending_irqs)) {
+        // Target privilege mode
+        uint8_t priv = PRIVILEGE_MACHINE;
+        uint32_t irqs = 0;
+        // Delegate pending IRQs from M to the highest possible mode
+        do {
+            irqs = pending_irqs & ~vm->csr.ideleg[priv];
+            pending_irqs &= vm->csr.ideleg[priv];
+            if (irqs) break;
+        } while (--priv);
+        // Skip if target priv < current priv, or equal and IRQs are disabled in status CSR
+        if (vm->priv_mode > priv) return;
+        if (vm->priv_mode == priv && !((1 << vm->priv_mode) & vm->csr.status)) return;
+
+        for (int i=11; i>=0; --i) {
+            if (irqs & (1 << i)) {
+                // Modify exception stack in csr.status
+                riscv_trap_priv_helper(vm, priv);
+                // Discard unfinished JIT block
+                riscv_jit_discard(vm);
+                // Switch privilege
+                riscv_switch_priv(vm, priv);
+                // Write exception info
+                vm->csr.epc[priv] = vm->registers[REGISTER_PC];
+                vm->csr.cause[priv] = i | riscv_cause_irq_mask(vm);
+                vm->csr.tval[priv] = 0;
+                // Jump to trap vector
+                if (vm->csr.tvec[priv] & 1) {
+                    vm->registers[REGISTER_PC] = (vm->csr.tvec[priv] & (~3ULL)) + (i << 2);
+                } else {
+                    vm->registers[REGISTER_PC] = vm->csr.tvec[priv] & (~3ULL);
+                }
+                return;
+            }
+        }
+    }
+    return;
+}
+
+void riscv_hart_run(rvvm_hart_t* vm)
+{
+    rvvm_info("Hart %p started", vm);
+
+    while (true) {
+        // Allow hart to run
+        atomic_store_uint32_ex(&vm->wait_event, HART_RUNNING, ATOMIC_RELAXED);
+
+        // Handle events
+        vm->csr.ip |= atomic_swap_uint32(&vm->pending_irqs, 0);
+        uint32_t events = atomic_swap_uint32(&vm->pending_events, 0);
+
+        if ((vm->csr.ip & (1U << INTERRUPT_MTIMER)) && !rvtimer_pending(&vm->timer)) {
+            vm->csr.ip &= ~(1U << INTERRUPT_MTIMER);
+        }
+
+        if (unlikely(events)) {
+            if (events & HART_EVENT_PAUSE) {
+                rvvm_info("Hart %p stopped", vm);
+                return;
+            }
+            if (events & HART_EVENT_PREEMPT) {
+                sleep_ms(atomic_swap_uint32(&vm->preempt_ms, 0));
+            }
+        }
+
+        riscv_handle_irqs(vm);
+
+        // Run the hart
+        riscv_run_till_event(vm);
+        if (vm->trap) {
+            vm->registers[REGISTER_PC] = vm->trap_pc;
+            vm->trap = false;
+        }
+    }
+}
+
+static void* riscv_hart_run_wrap(void* ptr)
+{
+    if (rvvm_getarg_int("noisolation") < 1) {
+        rvvm_restrict_this_thread();
+    }
+    riscv_hart_run((rvvm_hart_t*)ptr);
+    return NULL;
+}
+
+void riscv_hart_spawn(rvvm_hart_t *vm)
+{
+    atomic_store_uint32(&vm->pending_events, 0);
+    vm->thread = thread_create(riscv_hart_run_wrap, (void*)vm);
+}
+
+void riscv_hart_queue_pause(rvvm_hart_t* vm)
+{
+    atomic_or_uint32(&vm->pending_events, HART_EVENT_PAUSE);
+    riscv_hart_notify(vm);
 }
 
 void riscv_hart_pause(rvvm_hart_t* vm)
@@ -358,10 +372,4 @@ void riscv_hart_pause(rvvm_hart_t* vm)
     thread_ctx_t* thread = vm->thread;
     vm->thread = NULL;
     thread_join(thread);
-}
-
-void riscv_hart_queue_pause(rvvm_hart_t* vm)
-{
-    atomic_or_uint32(&vm->pending_events, EXT_EVENT_PAUSE);
-    riscv_hart_notify(vm);
 }
--- src/riscv_hart.h
+++ src/riscv_hart.h
@@ -21,25 +21,22 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include "rvvm.h"
 
-#define HART_STOPPED 0
-#define HART_RUNNING 1
+/*
+ * Hart context creation, disposal
+ */
 
 // Create hart context
 rvvm_hart_t* riscv_hart_init(rvvm_machine_t* machine);
-void riscv_hart_free(rvvm_hart_t* vm);
 
 // Prepare harts before spawning any of them
 void riscv_hart_prepare(rvvm_hart_t* vm);
 
-/* Hart-thread routines */
+// Free hart context
+void riscv_hart_free(rvvm_hart_t* vm);
 
-// Executes the hart in a current thread
-// Returns upon receiving EXT_EVENT_PAUSE
-void riscv_hart_run(rvvm_hart_t* vm);
-
-// Execute a userland context in current thread
-// Returns trap cause upon any CPU trap
-rvvm_addr_t riscv_hart_run_userland(rvvm_hart_t* vm);
+/*
+ * Hart operations, may be called ONLY on hart thread
+ */
 
 // Correctly applies side-effects of switching privileges
 void riscv_switch_priv(rvvm_hart_t* vm, uint8_t priv_mode);
@@ -47,41 +44,48 @@ void riscv_switch_priv(rvvm_hart_t* vm, uint8_t priv_mode);
 // Correctly applies side-effects of switching XLEN
 void riscv_update_xlen(rvvm_hart_t* vm);
 
-/*
- * Traps the hart
- * Should be the last operation before returning to dispatch
- * JIT may unwind to dispatch afterwards
- */ 
+// Traps the hart. Should be the last operation before returning to dispatch.
 void riscv_trap(rvvm_hart_t* vm, bitcnt_t cause, maxlen_t tval);
 
-// Used for wfi, returns true if an interrupt was caught
-bool riscv_handle_irqs(rvvm_hart_t* vm, bool wfi);
+/*
+ * Hart operations, may be called on any thread
+ */
 
-// Used in tlb flush routines to reset page_addr in dispatch
+// Makes vCPU return from riscv_run_till_event() to check for IRQs,
+// or after flushing pages overlapping PC (optimization quirk)
 void riscv_restart_dispatch(rvvm_hart_t* vm);
+
+// Signals interrupt to the hart
+void riscv_interrupt(rvvm_hart_t* vm, bitcnt_t irq);
+
+// Clears interrupt in IP csr of the hart
+void riscv_interrupt_clear(rvvm_hart_t* vm, bitcnt_t irq);
+
+// Signal the vCPU to check for timer interrupts
+void riscv_hart_check_timer(rvvm_hart_t* vm);
+
+// Preempt the hart vCPU thread from consuming CPU for preempt_ms
+void riscv_hart_preempt(rvvm_hart_t* vm, uint32_t preempt_ms);
+
+/*
+ * Running the hart
+ */
+
+// Executes the machine hart in a current thread
+// Returns upon receiving EXT_EVENT_PAUSE
+void riscv_hart_run(rvvm_hart_t* vm);
+
+// Execute a userland thread context in current thread
+// Returns trap cause upon any CPU trap
+rvvm_addr_t riscv_hart_run_userland(rvvm_hart_t* vm);
+
+// Spawns hart vCPU thread, returns immediately
+void riscv_hart_spawn(rvvm_hart_t *vm);
 
 // Requests the hart to be paused as soon as possible
 void riscv_hart_queue_pause(rvvm_hart_t* vm);
 
-/* External-thread routines */
-
-// Spawns thread for hart execution, returns immediately
-void riscv_hart_spawn(rvvm_hart_t *vm);
-
-// Signals interrupt to the hart, may be called anywhere
-void riscv_interrupt(rvvm_hart_t* vm, bitcnt_t irq_mask);
-
-// Clears interrupt in IP csr of the hart, may be called anywhere
-void riscv_interrupt_clear(rvvm_hart_t* vm, bitcnt_t irq_mask);
-
-// Forces hart to check timecmp register for interrupts
-void riscv_hart_check_timer(rvvm_hart_t* vm);
-
-// Preempt the hart from consuming CPU
-void riscv_hart_preempt(rvvm_hart_t* vm, uint32_t preempt_ms);
-
-// Pauses hart in a consistent state, terminates executing thread
-// This function is blocking
+// Pauses hart in a consistent state, terminates vCPU thread
 void riscv_hart_pause(rvvm_hart_t* vm);
 
 #endif
